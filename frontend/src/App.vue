7fd6d920a3ee90f16848bdde28fb31347b2c38f1
Ricardo Taverna <taverna.ricardo@gmail.com>
2021-12-20 21:37:35 -0300

    inicio do frontend.

---
+++ frontend/src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-app>
+      <v-app-bar
+        color="deep-purple"
+        dark
+      >
+        <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+        <div class="d-flex align-center">
+          <v-img
+            alt="Vuetify Name"
+            class="shrink mt-1 hidden-sm-and-down"
+            contain
+            min-width="100"
+            src="./assets/logo_name.png"
+          />
+        </div>
+      </v-app-bar>
+
+      <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        temporary
+      >
+        <v-list
+          nav
+          dense
+        >
+          <v-list-item-group
+            v-model="group"
+            active-class="deep-purple--text text--accent-4"
+          >
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Account</v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+
+    <v-main>
+      <HelloWorld/>
+      <wether-card/>
+      <Footer/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Footer from './components/Footer.vue';
+import HelloWorld from './components/HelloWorld';
+import WetherCard from './components/WetherCard';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+    WetherCard,
+    Footer
+  },
+
+  data: () => ({
+      drawer: false,
+      group: null,
+  }),
+};
+</script>
---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Bem vindo ao WNotificator
+        </h1>
+
+        <p class="subheading font-weight-regular">
+          Configure sua cidade e receba notificações personalizadas sobre o clima no seu WhatsApp.
+          <br>
+        </p>
+        <v-btn
+          color="indigo darken-1 white--text"
+          depressed
+          elevation="6"
+          x-large
+        >Criar Conta</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+    }),
+  }
+</script>
